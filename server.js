6e36c6eaa086e1b4002de5eb1e520b5cb48b2bc5
Bryan Yabut <bryan.yabut@outlook.com>
2026-01-19 19:49:52 -0500

    added an error handler

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 //TODO: Other required modules
+const errorHandler = require("./middleware/error");
 
 const PORT = process.env.PORT || 3000;
 
@@ -23,6 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 //TODO:Error Handling Middleware
+app.use(errorHandler);
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));

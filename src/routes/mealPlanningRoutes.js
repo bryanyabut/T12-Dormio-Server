a1977a51737c7c04bee534a70e44c0a12b56a9bd
Bryan Yabut <bryan.yabut@outlook.com>
2026-03-19 14:50:12 -0400

    added the GET endpoints for meal plans

--- src/routes/mealPlanningRoutes.js
+++ src/routes/mealPlanningRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { authenticateToken } = require('../middleware/authMiddleware');
-const { getAllMeals } = require('../controllers/mealPlanningController');
+const { getAllMealPlans, 
+    getMealPlanDetails, getMealsByDay} = require('../controllers/mealPlanningController');
 
-router.get('/', authenticateToken, getAllMeals);
+router.get('/', authenticateToken, getAllMealPlans);
+router.get('/:mealPlanTypeId', authenticateToken, getMealPlanDetails);
+router.get('/:mealPlanTypeId/day/:dayOfWeek', authenticateToken, getMealsByDay);
 
 module.exports = router;
